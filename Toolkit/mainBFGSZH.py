39de7b79fc36feff2ad19b225a9456f3d0ac30d8
Hero-DFT <124654042+hero-dft@users.noreply.github.com>
2023-02-07 14:12:44 +0800

    mainBFGSZH.py

---
+++ Toolkit/mainBFGSZH.py
@@ -0,0 +1,48 @@
+# 置零，改变的是变量的本身，无返回值
+import os
+import shutil
+from os import path
+
+import sys
+import ase.io
+
+from ase.calculators.vasp import Vasp
+
+from ZERO import BFGSZ,BFGSZH
+from myacal import traceAtoms
+
+vasp = Vasp(
+    directory='vasp',  # output dir
+    xc='pbe',  # the exchange-correlation functional
+
+    # 下面是incar的内容
+    lwave=False,
+    prec='Accurate',  # MP is Accurate
+    istart=0,
+    isym=1,
+    encut=300,
+    sigma=0.1,
+    algo='Fast',
+    nelm=1000,
+    # ediffg=-0.05,
+    ediff=1e-5,
+    npar=4,
+    ibrion=-1,
+    nsw=0,
+    
+)
+
+models = ['MGH']
+for model in models:
+    pts = ase.io.read(model + '.vasp')
+    pts = traceAtoms([i for i in pts], cell=pts.cell, pbc=True)
+    pts.calc = vasp
+    pts0 = pts.copy()
+    if not path.exists(model):
+        os.makedirs(model)
+    dyn = BFGSZH(pts, trajectory='{0}.traj'.format(model))
+    i = 1
+    while not dyn.run(steps=i, fmax=0.05):
+        i += 1
+
+
